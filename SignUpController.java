5b5dc9616bbbd91005894f2ffda9fd603a4ab884
Mai Samir <mai.samir.mohammad@gmail.com>
2022-12-31 23:21:16 +0200

    login and signup

---
+++ SignUpController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package version.pkg1.welcom.page;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class SignUpController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+     @FXML
+    private void GameScreen(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+      @FXML
+      private void homescreen(ActionEvent event) throws Exception{
+       Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+       Scene scene = new Scene(root); 
+       Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+       stage.setScene(scene);
+       stage.show();
+    }
+    
+}
