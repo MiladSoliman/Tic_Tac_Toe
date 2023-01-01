1f66c90d5d813e1fefa813faf71bfd37869fe450
Hadia Yehia <hadiayzmm@gmail.com>
2023-01-01 19:40:53 +0200

    server gui

---
+++ server/DataAccessLayerFinal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server_finalproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hadia
+ */
+public class DataAccessLayerFinal {
+    public static int insert (Player player) throws SQLException{
+        int result=0;
+    DriverManager.registerDriver(new org.apache.derby.jdbc.ClientDriver());
+    Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/finalproject","tictactoe","tictactoe");
+   PreparedStatement stm=con.prepareStatement("INSERT INTO PLAYERS VALUES (?,?,?,?,?)");
+     stm.setString(1, "hadia");
+     stm.setString(2, "hadia@gmail.com");
+     stm.setString(3,"1234" );
+     stm.setInt(4, 5);
+     stm.setString(5,"online" );
+     
+     result =stm.executeUpdate();
+     con.commit();
+     stm.close();
+     con.close();
+    return result;
+    }
+    }
---
+++ server/PlayersController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server_finalproject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author hadia
+ */
+public class PlayersController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+ 
+         
+       
+        
+      
+        
+        
+    
+}
