92c1d1cf80556bec1cffd25529c8739a5d4ab616
Hadia Yehia <hadiayzmm@gmail.com>
2023-01-04 21:16:12 +0200

    play again & draw games

--- finalproject/src/version/pkg1/welcom/page/SinglePlayerController.java
+++ finalproject/src/version/pkg1/welcom/page/SinglePlayerController.java
@@ -15,7 +15,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -29,6 +31,8 @@ public class SinglePlayerController implements Initializable {
     private Button homescreen;
     @FXML
     private Button gotolevels;
+    @FXML
+    private TextField single_player_name;
 
     /**
      * Initializes the controller class.
@@ -50,11 +54,30 @@ public class SinglePlayerController implements Initializable {
 
     @FXML
     private void gotolevels(ActionEvent event) throws IOException {
-         Parent root = FXMLLoader.load(getClass().getResource("LevelScreen.fxml"));
+        String username;
+        if(single_player_name.getText().toString().isEmpty())
+        {Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Required Fields Empty");
+            alert.setContentText("Text field must be filled "
+                    + "out.\nPlease try again.");
+            alert.showAndWait();
+           
+        }
+        else{
+         username = single_player_name.getText().toString();
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LevelScreen.fxml"));	
+        Parent root = loader.load();	
+	LevelScreenController levelScreenController = loader.getController();
+	levelScreenController.saveName(username);
+        
+       //Parent root = FXMLLoader.load(getClass().getResource("LevelScreen.fxml"));
        Scene scene = new Scene(root);
        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
        stage.setScene(scene);
        stage.show();
+        }
     }
     
 }
