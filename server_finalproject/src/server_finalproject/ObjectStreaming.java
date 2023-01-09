1e8fb3d0625a457cf152db3dca71fad2716d9068
Hadia Yehia <hadiayzmm@gmail.com>
2023-01-09 20:10:48 +0200

    connection between server and app

--- server_finalproject/src/server_finalproject/ObjectStreaming.java
+++ server_finalproject/src/server_finalproject/ObjectStreaming.java
@@ -5,9 +5,11 @@
  */
 package server_finalproject;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -18,39 +20,63 @@ import java.util.logging.Logger;
  * @author hadia
  */
 public class ObjectStreaming extends Thread{
-    Socket client;
-   ObjectOutputStream out;
-   ObjectInputStream in;
+   Socket client;
+   //ObjectOutputStream out;
+   //ObjectInputStream in;
    ServerSocket server;
-   Thread th =new Thread(this);
+   Thread th;
+   DataInputStream dis;
+    PrintStream ps;
 
     public ObjectStreaming() {
-       while(true){
+        
+      
+      
         try {
-            this.server = new ServerSocket(5000);
-            client=server.accept();
-            out =new ObjectOutputStream(client.getOutputStream());
-            in =new ObjectInputStream(client.getInputStream());
-            start();
+             server = new ServerSocket(5005);
+            
+             th=new Thread(new Runnable() {
+                 @Override
+                 public void run() {
+                    
+            while(true){
+                     try {
+                         
+                         client=server.accept();
+                         dis=new DataInputStream(client.getInputStream());
+                         ps=new PrintStream(client.getOutputStream());
+                         String str=dis.readLine();
+                         System.out.println(str);
+                         //SignUp obj=(SignUp) in.readObject();
+                //System.out.println("username="+obj.getUsername());
+                     } catch (IOException ex) { 
+                    Logger.getLogger(ObjectStreaming.class.getName()).log(Level.SEVERE, null, ex);
+                } 
+                 }
+                 }
+             });
+             
+             th.start();
             //streaming(client);
             /*out.close();
             in.close();
             client.close();
             server.close();*/
-        } catch (IOException ex) {
-            Logger.getLogger(ObjectStreaming.class.getName()).log(Level.SEVERE, null, ex);
-        }
-            }  
-    }
+         
+           // }  
+    }  catch (IOException ex) { 
+           Logger.getLogger(ObjectStreaming.class.getName()).log(Level.SEVERE, null, ex);
+       } 
 
-    public void run()
+    /*public void run()
     {
     
-        while(true)
-        {
-        
+        //while(true)
+        //{
+         
             
             try {
+               
                 
                 SignUp obj=(SignUp) in.readObject();
                 System.out.println("username="+obj.getUsername());
@@ -59,8 +85,9 @@ public class ObjectStreaming extends Thread{
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(ObjectStreaming.class.getName()).log(Level.SEVERE, null, ex);
             }
-            } 
-        }
+            //}
+        }*/
     }
+}
     
 
